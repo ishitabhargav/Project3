d563b56f30f6e21cf8c8057f7f5ea66671e788c4
Ishita <bhargav.ishi@gmail.com>
2023-11-30 22:33:32 -0500

    changes after 11/30 meeting

--- algorithmic_alchemy.py
+++ algorithmic_alchemy.py
@@ -7,6 +7,12 @@ def sigmoid(z) -> int:
     return 1 / (1 + np.exp(z * -1))
 
 
+def log_loss(dataset, w) -> int:
+    loss = 0
+    for x in dataset:
+        y = x.isDangerous
+        loss = loss + ((-1 * y)*np.log((sigmoid(np.dot(x, w)))) - (1 - y)*np.log(1 - sigmoid(np.dot(x, w))))
+    return (1/len(dataset)) * loss
 
 
 class AlgorithmicAlchemy:
@@ -14,3 +20,6 @@ class AlgorithmicAlchemy:
         weights = np.zeros(400)
         for count in range(400):
             weights[count] = random.random()
+        dataset = []
+        for count in range(500):
+            dataset.append(Wiring())
