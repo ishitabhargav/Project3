86dc6d8dcc5c6e92712b54dd9c45bec7be9e356b
sofiaj2 <sej59@scarletmail.rutgers.edu>
2023-12-05 20:46:43 -0500

    changes after 12/5

--- algorithmic_alchemy.py
+++ algorithmic_alchemy.py
@@ -16,9 +16,19 @@ def sum_log_loss(dataset, w) -> int:
     return (1 / len(dataset)) * loss
 
 
-def log_loss(x, y, w) -> int:
-    sigmoid_output = sigmoid(np.dot(x, w))
-    return (-1 * y) * np.log(sigmoid_output) - (1 - y) * np.log(1 - sigmoid_output)
+def log_loss(x, y, w, time) -> int:
+    dot_product = np.dot(x, w)
+    sigmoid_output = sigmoid(dot_product)
+
+    if sigmoid_output <= 0:
+        print('orange')
+    elif 1 - sigmoid_output <= 0:
+        print('papaya')
+    else:
+        print("successful")
+    ans = (-1 * y) * np.log(sigmoid_output) - (1 - y) * np.log(1 - sigmoid_output)
+
+    return ans
 
 
 def calculate_gradient(x, y, w) -> int:
@@ -29,20 +39,22 @@ def stochastic_gradient_descent(dataset, alpha):
     weights_length = len(dataset[0][0])  # get the length of an input vector
     weights = np.zeros(weights_length)
     for count in range(weights_length):
-        weights[count] = random.random()
+        weights[count] = random.uniform(0, 0.25)  # unit intervals b/w -9 to -3 work well
     time = 0
-    termination = 1000
+    termination = 350000
     loss_list = []
+    # print("starting SGD")
     while time < termination:
         # 1. pick a data point at random
         data_point = dataset[np.random.randint(0, len(dataset))]
         x = data_point[0]  # vector
         y = data_point[1]  # classification
         # 2. update weights vector
-        loss = log_loss(x, y, weights)
+        loss = log_loss(x, y, weights, time)
         loss_list.append(loss)
         updated_weights = weights - (alpha * calculate_gradient(x, y, weights) * x)
         weights = updated_weights
+        time = time + 1
 
     return [weights, loss_list]
 
@@ -54,7 +66,7 @@ class AlgorithmicAlchemy:
         for count in range(training_dataset_size):
             data_point = Wiring()
             dataset.append((data_point.vector, data_point.is_dangerous))  # input, output pairing
-        alpha = 1
+        alpha = 0.1
         stochastic_gradient_output = stochastic_gradient_descent(dataset, alpha)
         self.weights = stochastic_gradient_output[0]
         self.loss_list = stochastic_gradient_output[1]
@@ -73,8 +85,69 @@ def main():
         validation_set.append((data_point.vector, data_point.is_dangerous))
 
     # give model input and get output for data points in validation dataset
-    output_500 = []
+
+    performance_hashtable = {}
     # classify input wirings in testing dataset
+    threshold_list = np.linspace(0, 1, 50)
+    for threshold in threshold_list:
+        performance_hashtable[threshold] = 0
+    for threshold in threshold_list:
+        output_500 = []
+        for data_point in validation_set:
+            x = data_point[0]
+            sigmoid_output = sigmoid(np.dot(x, weights_500))
+            if sigmoid_output < threshold:
+                output_500.append(0)
+            else:
+                output_500.append(1)
+
+        # calculate performance
+        num_correct = 0
+        for count in range(model_1_size):
+            output_model = output_500[count]
+            output_answer = validation_set[count][1]
+            if output_answer == output_model:
+                num_correct = num_correct + 1
+        performance_hashtable[threshold] = num_correct / model_1_size
+
+    iteration_values = np.arange(start=0, stop=350000, step=1)
+    y_vals = []
+    for item in performance_hashtable:
+        y_vals.append(performance_hashtable[item])
+
+    for w in weights_500:
+        print(w)
+
+    plt.subplot(1, 2, 1)  # First subplot of performance and threshold values
+    plt.plot(threshold_list, y_vals, marker='o')
+    plt.xlabel("threshold")
+    plt.ylabel("performance")
+    plt.title('Performance as a Function of Threshold')
+
+    plt.subplot(1, 2, 2)  # Second subplot of loss
+    plt.plot(iteration_values, algorithmic_alchemy_500.loss_list, marker='o')
+    plt.xlabel("Iteration Values")
+    plt.ylabel("Loss Function Values")
+    plt.title("Loss Function During Training")
+    plt.legend(loc='upper right')
+    # Adjust layout to prevent subplot overlap
+    plt.tight_layout()
+
+    # Display the plot
+    plt.show()
+
+    '''plt.plot(iteration_values, algorithmic_alchemy_500.loss_list, label='Line Graph')
+    plt.xlabel("Iteration Values")
+    plt.ylabel("Loss Function Values")
+    plt.title("Loss Function During Training")
+    plt.legend(loc='upper right')
+    plt.show()'''
+
+
+if __name__ == "__main__":
+    main()
+
+'''
     for data_point in validation_set:
         x = data_point[0]
         y = data_point[1]
@@ -94,9 +167,12 @@ def main():
     performance = num_correct / model_1_size
     print(performance)
 
-    plt.plot(algorithmic_alchemy_500.loss_list)
+
+    iteration_values = np.arange(start=0, stop=100000, step=1)
+    plt.plot(iteration_values, algorithmic_alchemy_500.loss_list, label='Line Graph')
+    plt.xlabel("Iteration Values")
+    plt.ylabel("Loss Function Values")
+    plt.title("Loss Function During Training")
+    plt.legend(loc='upper right')
     plt.show()
-
-
-if __name__ == "__main__":
-    main()
+'''
--- wiring.py
+++ wiring.py
@@ -4,121 +4,125 @@ import numpy as np
 
 class Wiring:
     def __init__(self):
-        image_length = 80 # 20*4 because each pixel is represented by 4 digits
-        self.vector = np.zeros(1601) #image_length**2 + 1
-        self.vector[0] = 1 # first index of entire vector/image should be 1
+        image_length = 20
+        self.vector = np.zeros(image_length ** 2 * 4 + 1)  # each pixel represented by 4 digits
+        self.vector[0] = 1  # first index of entire vector/image should be 1
         red = [1, 0, 0, 0]
         blue = [0, 1, 0, 0]
         yellow = [0, 0, 1, 0]
         green = [0, 0, 0, 1]
         self.colors = [red, blue, yellow, green]
 
-        self.is_dangerous = 0 # false
+        self.is_dangerous = 0  # false
         # if < 0.5, pick row first, otherwise pick col first
         row_or_col_first = random.random()
-        print(row_or_col_first)
+        # print(row_or_col_first)
 
         if row_or_col_first < 0.5:
             # 1. pick first row to color in
-            rand_row_1 = np.random.randint(0, 20)
-            print("picking first row: " + str(rand_row_1))
+            rand_row_1 = np.random.randint(0, image_length)
+            #print("picking first row: " + str(rand_row_1))
             rand_color_1 = self.colors.pop(np.random.randint(0, len(self.colors)))
             if rand_color_1 == red:
-                self.is_dangerous = 1 # true
-            for count in range(rand_row_1 * image_length + 1, rand_row_1 * image_length + image_length + 1):
+                self.is_dangerous = 1  # true
+            for count in range(rand_row_1 * (image_length * 4) + 1,
+                               rand_row_1 * (image_length * 4) + (image_length * 4) + 1, 4):
                 self.vector[count] = rand_color_1[0]
-                self.vector[count+1] = rand_color_1[1]
-                self.vector[count+2] = rand_color_1[2]
-                self.vector[count+3] = rand_color_1[3]
+                self.vector[count + 1] = rand_color_1[1]
+                self.vector[count + 2] = rand_color_1[2]
+                self.vector[count + 3] = rand_color_1[3]
 
             # 2. pick first column to color in
-            rand_col_1 = np.random.randint(0, 20)
+            rand_col_1 = np.random.randint(0, image_length)
             rand_color_2 = self.colors.pop(np.random.randint(0, len(self.colors)))
             if not self.is_dangerous and rand_color_1 != yellow and rand_color_2 == red:
-                self.is_dangerous = 1 # true
-            for count in range(rand_col_1 + 1, len(self.vector), image_length):
+                self.is_dangerous = 1  # true
+            for count in range(rand_col_1 + 1, len(self.vector), image_length * 4):
                 self.vector[count] = rand_color_2[0]
-                self.vector[count+1] = rand_color_2[1]
-                self.vector[count+2] = rand_color_2[2]
-                self.vector[count+3] = rand_color_2[3]
+                self.vector[count + 1] = rand_color_2[1]
+                self.vector[count + 2] = rand_color_2[2]
+                self.vector[count + 3] = rand_color_2[3]
 
             # 3. pick second row to color in
-            rand_row_2 = np.random.randint(0, 20)
+            rand_row_2 = np.random.randint(0, image_length)
             while rand_row_2 == rand_row_1:
-                rand_row_2 = np.random.randint(0, 20)
+                rand_row_2 = np.random.randint(0, image_length)
             rand_color_3 = self.colors.pop(np.random.randint(0, len(self.colors)))
             if not self.is_dangerous and rand_color_1 != yellow and rand_color_2 != yellow and rand_color_3 == red:
                 self.is_dangerous = 1
-            for count in range(rand_row_2 * image_length + 1, rand_row_2 * image_length + image_length + 1):
+            for count in range(rand_row_2 * (image_length * 4) + 1,
+                               rand_row_2 * (image_length * 4) + (image_length * 4) + 1, 4):
                 self.vector[count] = rand_color_3[0]
-                self.vector[count+1] = rand_color_3[1]
-                self.vector[count+2] = rand_color_3[2]
-                self.vector[count+3] = rand_color_3[3]
+                self.vector[count + 1] = rand_color_3[1]
+                self.vector[count + 2] = rand_color_3[2]
+                self.vector[count + 3] = rand_color_3[3]
 
             # 4. pick second column to color in
-            rand_col_2 = np.random.randint(0, 20)
+            rand_col_2 = np.random.randint(0, image_length)
             while rand_col_2 == rand_col_1:
-                rand_col_2 = np.random.randint(0, 20)
+                rand_col_2 = np.random.randint(0, image_length)
             rand_color_4 = self.colors.pop(np.random.randint(0, len(self.colors)))
-            for count in range(rand_col_2 + 1, len(self.vector), image_length):
+            for count in range(rand_col_2 + 1, len(self.vector), image_length * 4):
                 self.vector[count] = rand_color_4[0]
-                self.vector[count+1] = rand_color_4[1]
-                self.vector[count+2] = rand_color_4[2]
-                self.vector[count+3] = rand_color_4[3]
+                self.vector[count + 1] = rand_color_4[1]
+                self.vector[count + 2] = rand_color_4[2]
+                self.vector[count + 3] = rand_color_4[3]
 
         else:
             # 1. pick first column to color in
-            rand_col_1 = np.random.randint(0, 20)
-            print("picking first col: " + str(rand_col_1))
+            rand_col_1 = np.random.randint(0, image_length)
+            #print("picking first col: " + str(rand_col_1))
             rand_color_1 = self.colors.pop(np.random.randint(0, len(self.colors)))
             if rand_color_1 == red:
                 self.is_dangerous = 1
-            for count in range(rand_col_1 + 1, len(self.vector), image_length):
+            for count in range(rand_col_1 + 1, len(self.vector), image_length * 4):
                 self.vector[count] = rand_color_1[0]
-                self.vector[count+1] = rand_color_1[1]
-                self.vector[count+2] = rand_color_1[2]
-                self.vector[count+3] = rand_color_1[3]
+                self.vector[count + 1] = rand_color_1[1]
+                self.vector[count + 2] = rand_color_1[2]
+                self.vector[count + 3] = rand_color_1[3]
 
             # 2. pick first row to color in
-            rand_row_1 = np.random.randint(0, 20)
+            rand_row_1 = np.random.randint(0, image_length)
             rand_color_2 = self.colors.pop(np.random.randint(0, len(self.colors)))
             if not self.is_dangerous and rand_color_1 != yellow and rand_color_2 == red:
                 self.is_dangerous = 1
-            for count in range(rand_row_1 * image_length + 1, rand_row_1 * image_length + image_length + 1):
+            for count in range(rand_row_1 * (image_length * 4) + 1,
+                               rand_row_1 * (image_length * 4) + (image_length * 4) + 1, 4):
                 self.vector[count] = rand_color_2[0]
-                self.vector[count+1] = rand_color_2[1]
-                self.vector[count+2] = rand_color_2[2]
-                self.vector[count+3] = rand_color_2[3]
+                self.vector[count + 1] = rand_color_2[1]
+                self.vector[count + 2] = rand_color_2[2]
+                self.vector[count + 3] = rand_color_2[3]
 
             # 3. pick second column to color in
-            rand_col_2 = np.random.randint(0, 20)
+            rand_col_2 = np.random.randint(0, image_length)
             while rand_col_2 == rand_col_1:
-                rand_col_2 = np.random.randint(0, 20)
+                rand_col_2 = np.random.randint(0, image_length)
             rand_color_3 = self.colors.pop(np.random.randint(0, len(self.colors)))
             if not self.is_dangerous and rand_color_1 != yellow and rand_color_2 != yellow and rand_color_3 == red:
                 self.is_dangerous = 1
-            for count in range(rand_col_2 + 1, len(self.vector), image_length):
+            for count in range(rand_col_2 + 1, len(self.vector), image_length * 4):
                 self.vector[count] = rand_color_3[0]
-                self.vector[count+1] = rand_color_3[1]
-                self.vector[count+2] = rand_color_3[2]
-                self.vector[count+3] = rand_color_3[3]
+                self.vector[count + 1] = rand_color_3[1]
+                self.vector[count + 2] = rand_color_3[2]
+                self.vector[count + 3] = rand_color_3[3]
 
             # 4. pick second row to color in
-            rand_row_2 = np.random.randint(0, 20)
+            rand_row_2 = np.random.randint(0, image_length)
             while rand_row_2 == rand_row_1:
-                rand_row_2 = np.random.randint(0, 20)
+                rand_row_2 = np.random.randint(0, image_length)
             rand_color_4 = self.colors.pop(np.random.randint(0, len(self.colors)))
-            for count in range(rand_row_2 * image_length + 1, rand_row_2 * image_length + image_length + 1):
+            for count in range(rand_row_2 * (image_length * 4) + 1,
+                               rand_row_2 * (image_length * 4) + (image_length * 4) + 1, 4):
                 self.vector[count] = rand_color_4[0]
-                self.vector[count+1] = rand_color_4[1]
-                self.vector[count+2] = rand_color_4[2]
-                self.vector[count+3] = rand_color_4[3]
+                self.vector[count + 1] = rand_color_4[1]
+                self.vector[count + 2] = rand_color_4[2]
+                self.vector[count + 3] = rand_color_4[3]
 
 
-image = Wiring()
+'''image = Wiring()
 
-for count in range(400):
+for count in range(1, len(image.vector)):
     print(int(image.vector[count]), end=" ")
-    if (count + 1) % 20 == 0:
+    if count % 80 == 0:
         print("")
-print(image.is_dangerous)
+print(image.is_dangerous)'''
